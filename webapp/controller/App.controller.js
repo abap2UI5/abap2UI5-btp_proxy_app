925df25f89d0942c0bc5745fcfd77b35a732ac67
Oblomov Dev <oblomov-dev@outlook.com>
2023-12-05 12:42:41 +0000

    update

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -6,29 +6,7 @@ sap.ui.define(
       "use strict";
   
       return BaseController.extend("zabap2ui5.controller.App", {
-        onInit: function() {
 
-          debugger;
-          //var a = "sap/bc/srt/scs_ext/sap/journalentrybulkchangerequest_?MessageId=" + this._createUuid();
-          var a = "sap/bc/ZTEST";
-          jQuery.ajax({
-              url: a,
-              type: "POST",
-              dataType: "text",
-              data: "",
-              async: true,
-              contentType: "text/html; charset=utf-8",
-              success: function(t, a, s) {
-                  debugger;
-                  var dummy = "test";
-              },
-              error: function(e) {
-                debugger;
-                var dummy2 = "test";
-              }
-          })
-
-        }
       });
     }
   );
--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,76 +1,57 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+  "sap/ui/core/mvc/Controller"
 ],
-    /**
-     * @param {typeof sap.ui.core.mvc.Controller} Controller
-     */
-    function (Controller) {
-        "use strict";
+  /**
+   * @param {typeof sap.ui.core.mvc.Controller} Controller
+   */
+  function (Controller) {
+    "use strict";
 
-        return Controller.extend("zabap2ui5.controller.View1", {
-            onInit: function () {                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
-                sap.ui.core.BusyIndicator.show();  
-                
+    return Controller.extend("zabap2ui5.controller.View1", {
+      onInit: function () {
+        sap.ui.core.BusyIndicator.show();
+      },
 
+      onAfterRendering: function () {
 
+        sap.z2ui5 = {};
+        sap.z2ui5.pathname = this.getView().getModel().sServiceUrl;
+        try {
+          sap.z2ui5.oParent = this.oView.getParent();
+          if (sap.z2ui5.oParent.getMetadata().getName() !== 'sap.m.App') {
+            sap.z2ui5.oParent = this.getView().byId(this.getView().getId() + "--app");
+          }
+        } catch (error) {
+          sap.z2ui5.oParent = this.getView().byId(this.getView().getId() + "--app");
+        }
+        try {
+          sap.z2ui5.APP_START = this.getOwnerComponent().getComponentData().startupParameters.app_start[0];
+        } catch (e) { }
 
-              },                                                                                                                                                                                                                                                       
-                                                                                                                                                                                                                                                                       
-              onAfterRendering: function () {                                                                                                                                                                                                                          
-                                                                                                                                                                                                                                                                       
-                sap.z2ui5 = {};                                                                                                                                                                                                                                        
-                try {                                                                                                                                                                                                                                                  
-                  sap.z2ui5.oParent = this.oView.getParent();                                                                                                                                                                                                          
-                  if (sap.z2ui5.oParent.getMetadata().getName() !== 'sap.m.App') {                                                                                                                                                                                     
-                    sap.z2ui5.oParent = this.getView().byId(this.getView().getId() + "--app");                                                                                                                                                                         
-                  }                                                                                                                                                                                                                                                    
-                } catch (error) {                                                                                                                                                                                                                                      
-                  sap.z2ui5.oParent = this.getView().byId(this.getView().getId() + "--app");                                                                                                                                                                           
-                }                                                                                                                                                                                                                                                      
-                try {                                                                                                                                                                                                                                                  
-                  sap.z2ui5.APP_START = this.getOwnerComponent().getComponentData().startupParameters.app_start[0];                                                                                                                                                    
-                } catch (e) {}                                                                                                                                                                                                                                         
-                                                                                                                                                                                                                                                                       
-                this.getView().getModel().read("/z2ui5_odataCollection", {                                                                                                                                                                                             
-                  success: function (oRetrievedResult) {                                                                                                                                                                                                               
-                    var lv_html = oRetrievedResult.results[0].data;                                                                                                                                                                                                    
-                    var code = lv_html.split('<abc/>')[1];                                                                                                                                                                                                             
-                    sap.ui.controller("z2ui5_dummy_controller", {});                                                                                                                                                                                                   
-                    var xml =                                                                                                                                                                                                                                          
-                      "<mvc:View controllerName='z2ui5_dummy_controller' xmlns='http://www.w3.org/1999/xhtml' xmlns:mvc='sap.ui.core.mvc' >" +                                                                                                                         
-                      code + "</mvc:View>";                                                                                                                                                                                                                            
-                    var oView = new sap.ui.core.mvc.View.create({                                                                                                                                                                                                      
-                      type: 'XML',                                                                                                                                                                                                                                     
-                      definition: xml,                                                                                                                                                                                                                                 
-                    }).then(oView => {                                                                                                                                                                                                                                 
-                      sap.z2ui5.oParent.removeAllPages();                                                                                                                                                                                                              
-                      sap.z2ui5.oParent.insertPage(oView);                                                                                                                                                                                                             
-                      sap.z2ui5.oView = oView;                                                                                                                                                                                                                         
-                    });                                                                                                                                                                                                                                                
-                  }                                                                                                                                                                                                                                                    
-                });                                                                                                                                                                                                                                                    
-                                                                                                                                                                                                                                                                       
-                sap.z2ui5.ODataModel = this.getView().getModel();                                                                                                                                                                                                      
-                sap.z2ui5.readOData = function () {                                                                                                                                                                                                                    
-                                                                                                                                                                                                                                                                       
-                  var la_filters = [];                                                                                                                                                                                                                                 
-                  la_filters.push(new sap.ui.model.Filter({                                                                                                                                                                                                            
-                    path: "data",                                                                                                                                                                                                                                      
-                    operator: sap.ui.model.FilterOperator.EQ,                                                                                                                                                                                                          
-                    value1: JSON.stringify(sap.z2ui5.oBody)                                                                                                                                                                                                            
-                  }));                                                                                                                                                                                                                                                 
-                                                                                                                                                                                                                                                                       
-                  sap.z2ui5.ODataModel.read("/z2ui5_odataCollection", {                                                                                                                                                                                                
-                    filters: la_filters,                                                                                                                                                                                                                               
-                    success: function (oRetrievedResult) {                                                                                                                                                                                                             
-                      var lv_data = oRetrievedResult.results[0].data;                                                                                                                                                                                                  
-                      sap.z2ui5.oController.responseSuccess(lv_data);                                                                                                                                                                                                  
-                    },                                                                                                                                                                                                                                                 
-                    error: function (oError) {                                                                                                                                                                                                                         
-                      sap.z2ui5.oController.responseError(oError.responseText);                                                                                                                                                                                        
-                    }                                                                                                                                                                                                                                                  
-                  })                                                                                                                                                                                                                                                   
-                }                                                                                                                                                                                                                                                      
-              },                                                                                                                                                                                                                                                       
-            });                                                                                                                                                                                                                                                        
-          });                                                                                                                                                                                                                                                          
+        jQuery.ajax({
+          url: sap.z2ui5.pathname,
+          type: "GET",
+          dataType: "text",
+          data: "",
+          async: true,
+          contentType: "text/html; charset=utf-8",
+          success: function (data, a, s) {
+            let code = data.split('<abc/>')[1];
+            sap.ui.controller("z2ui5_dummy_controller", {});
+            let xml =
+              "<mvc:View controllerName='z2ui5_dummy_controller' xmlns='http://www.w3.org/1999/xhtml' xmlns:mvc='sap.ui.core.mvc' >" +
+              code + "</mvc:View>";
+            new sap.ui.core.mvc.View.create({
+              type: 'XML',
+              definition: xml,
+            }).then(oView => {
+              sap.z2ui5.oParent.removeAllPages();
+              sap.z2ui5.oParent.insertPage(oView);
+              sap.z2ui5.oView = oView;
+            });
+          }
+        })
+
+      },
+    });
+  });                                                                                                                                                                                                                                                          
